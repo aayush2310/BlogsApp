afd74a7a6a8d9105281b95be80b2092417f732d3
aayush2310 <99485539+aayush2310@users.noreply.github.com>
2022-08-05 23:24:55 +0530

    Create global.js

---
+++ global.js
@@ -0,0 +1,9 @@
+var testInstance=require('./test');
+
+console.log(testInstance.greet('Mohan'));
+testInstance.writeFile;
+testInstance.createFolder;
+testInstance.deletionOperation;
+testInstance.deletingFolders;
+
+//const dbUri='mongodb+srv://sample1:test123@nodecluster.drbwr.mongodb.net/node-tuts?retryWrites=true&w=majority';
